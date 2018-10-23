38cb38891ada0ea6f276e10e2aa9e59fbe68a0f0
Jesse <jmmichel@mit.edu>
2018-10-23 15:38:33 -0400

    a little bit of code cleanup

--- src/mnist_model.js
+++ src/mnist_model.js
@@ -22,25 +22,13 @@ function createModel() {
 }
 
 /**
- * Creates a model 
- * 
- * @returns {tf.Model} An instance of tf.Model.
- */
-function createDenseModel() {
-  const model = tf.sequential();
-  model.add(tf.layers.flatten({inputShape: [IMAGE_H, IMAGE_W, 1]}));
-  model.add(tf.layers.dense({units: 42, activation: 'relu'}));
-  model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
-  return model;
-}
-
-/**
  * Compile and train the given model.
  *
  * @param {*} model The model to
  */
 async function train(model) {
-  ui.logStatus('Training model...');
+  // TODO: start method
+  // ui.logStatus('Training model...');
 
   const LEARNING_RATE = 0.01;
   const optimizer = 'rmsprop';
@@ -54,7 +42,8 @@ async function train(model) {
   const batchSize = 64;
   const validationSplit = 0.15;
 
-  const trainEpochs = ui.getTrainEpochs();
+  // const trainEpochs = ui.getTrainEpochs();
+  const trainEpochs = 12;
 
   // We'll keep a buffer of loss and accuracy values over time.
   let trainBatchCount = 0;
@@ -72,31 +61,34 @@ async function train(model) {
     validationSplit,
     epochs: trainEpochs,
     callbacks: {
-      onBatchEnd: async (batch, logs) => {
-        trainBatchCount++;
-        ui.logStatus(
-            `Training... (` +
-            `${(trainBatchCount / totalNumBatches * 100).toFixed(1)}%` +
-            ` complete). To stop training, refresh or close page.`);
-        ui.plotLoss(trainBatchCount, logs.loss, 'train');
-        ui.plotAccuracy(trainBatchCount, logs.acc, 'train');
-        await tf.nextFrame();
-      },
-      onEpochEnd: async (epoch, logs) => {
-        valAcc = logs.val_acc;
-        ui.plotLoss(trainBatchCount, logs.val_loss, 'validation');
-        ui.plotAccuracy(trainBatchCount, logs.val_acc, 'validation');
-        await tf.nextFrame();
-      }
+      // TODO: ADD LOGGING!
+      // onBatchEnd: async (batch, logs) => {
+      //   trainBatchCount++;
+      //   ui.logStatus(
+      //       `Training... (` +
+      //       `${(trainBatchCount / totalNumBatches * 100).toFixed(1)}%` +
+      //       ` complete). To stop training, refresh or close page.`);
+      //   ui.plotLoss(trainBatchCount, logs.loss, 'train');
+      //   ui.plotAccuracy(trainBatchCount, logs.acc, 'train');
+      //   await tf.nextFrame();
+      // },
+      // onEpochEnd: async (epoch, logs) => {
+      //   valAcc = logs.val_acc;
+      //   ui.plotLoss(trainBatchCount, logs.val_loss, 'validation');
+      //   ui.plotAccuracy(trainBatchCount, logs.val_acc, 'validation');
+      //   await tf.nextFrame();
+      // }
     }
   });
 
   const testResult = model.evaluate(testData.xs, testData.labels);
   const testAccPercent = testResult[1].dataSync()[0] * 100;
   const finalValAccPercent = valAcc * 100;
-  ui.logStatus(
-      `Final validation accuracy: ${finalValAccPercent.toFixed(1)}%; ` +
-      `Final test accuracy: ${testAccPercent.toFixed(1)}%`);
+  // TODO: Add a termination message
+  // ui.logStatus(
+  //     `Final validation accuracy: ${finalValAccPercent.toFixed(1)}%; ` +
+  //     `Final test accuracy: ${testAccPercent.toFixed(1)}%`);
+
 }
 
 /**
@@ -116,22 +108,9 @@ async function showPredictions(model) {
     const axis = 1;
     const labels = Array.from(examples.labels.argMax(axis).dataSync());
     const predictions = Array.from(output.argMax(axis).dataSync());
-
-    ui.showTestResults(examples, predictions, labels);
+    // TODO: Display results
+    // ui.showTestResults(examples, predictions, labels);
   });
-}
-
-function createModel() {
-  let model;
-  const modelType = ui.getModelTypeId();
-  if (modelType === 'ConvNet') {
-    model = createConvModel();
-  } else if (modelType === 'DenseNet') {
-    model = createDenseModel();
-  } else {
-    throw new Error(`Invalid model type: ${modelType}`);
-  }
-  return model;
 }
 
 let data;
@@ -142,16 +121,16 @@ async function load() {
 
 // This is our main function. It loads the MNIST data, trains the model, and
 // then shows what the model predicted on unseen test data.
-ui.setTrainButtonCallback(async () => {
-  ui.logStatus('Loading MNIST data...');
-  await load();
+// ui.setTrainButtonCallback(async () => {
+//   ui.logStatus('Loading MNIST data...');
+//   await load();
 
-  ui.logStatus('Creating model...');
-  const model = createModel();
-  model.summary();
+//   ui.logStatus('Creating model...');
+//   const model = createModel();
+//   model.summary();
 
-  ui.logStatus('Starting model training...');
-  await train(model);
+//   ui.logStatus('Starting model training...');
+//   await train(model);
 
-  showPredictions(model);
-});
+//   showPredictions(model);
+// });
