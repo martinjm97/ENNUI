592691e8309fd8adbe4d4e1fbecc7a88bf38dafc
Jesse <jmmichel@mit.edu>
2018-11-02 01:13:57 -0400

    cleaned draggable

--- src/ui/shapes/draggable.ts
+++ src/ui/shapes/draggable.ts
@@ -4,9 +4,5 @@ export abstract class Draggable extends Shape {
     static readonly snapRadius: number = 400;
 
     static readonly defaultLocation: Point = new Point(50,100);
-    // let layerDefaults = {
-// 	x:50,
-// 	y:100,
-// }
 
 }
