341c73e806945329dd27067ec00f87e51b008ff8
Zack Holbrook <zackh@mit.edu>
2018-11-07 00:36:06 -0500

    comment

--- src/ui/blocks.ts
+++ src/ui/blocks.ts
@@ -1,37 +1,5 @@
 
 
-
-
-
-
-
-
-// let qwertx = 10
-// let qwerty = 20
-
-// let layerRectData = {
-// 	conv2D : {
-// 		page1 : [-54,-80,50,50,colors.layer.conv2D.page1],
-// 		page2 : [-37,-60,50,50,colors.layer.conv2D.page2],
-// 		page3 : [-20,-40,50,50,colors.layer.conv2D.page3],
-// 		hole: [0,0,10,10,'#eee'],
-// 	},
-// 	dense : {
-// 		main : [-8,-90,26,100,colors.layer.dense.main],
-// 		hole: [0,0,10,10,'#eee'],
-// 	},
-// 	maxPooling2D : {
-// 		page1 : [qwertx-54,qwerty-80,30,30,colors.layer.maxPooling2D.page1],
-// 		page2 : [qwertx-37,qwerty-60,30,30,colors.layer.maxPooling2D.page2],
-// 		page3 : [qwertx-20,qwerty-40,30,30,colors.layer.maxPooling2D.page3],
-// 		hole: [0,0,10,10,'#eee'],
-// 	},
-// }
-
-// let layerPortData = {
-// 	activation : [0,0]
-// }
-
 // let wireDefaults = {
 // 	x:200,
 // 	y:200
