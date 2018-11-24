8753380943d8e38a491a6991a020947b1d752718
Zack Holbrook <zackh@mit.edu>
2018-11-24 16:46:39 -0500

    lines

--- src/ui/shapes/activation.ts
+++ src/ui/shapes/activation.ts
@@ -3,22 +3,39 @@ import { Point, Rectangle } from "./shape";
 import { ActivationLayer } from "./layer";
 import * as d3 from "d3"
 import { windowProperties } from "../window";
+import { PassThrough } from "stream";
 
 export abstract class Activation extends Draggable {
 
     layer: ActivationLayer = null;
     abstract activationType: String;
     defaultLocation: Point = new Point(50,150);
+    body: d3.Selection<SVGGraphicsElement, {}, HTMLElement, any>;
 
     constructor(color: string) { 
         super();
-        let middleTooth: Rectangle = new Rectangle(new Point(0, 0), 10, 10, color);
-        let lowerBlock: Rectangle = new Rectangle(new Point(-8, 10), 26, 10, color);
 
-        this.svgComponent.call(lowerBlock.svgAppender.bind(lowerBlock))
-        this.svgComponent.call(middleTooth.svgAppender.bind(middleTooth))
+        this.body = this.svgComponent.append<SVGGraphicsElement>("path").attr("d", "M0 0 h10 v10 h8 v20 h-26 v-20 h8 v-10 Z")
+                                                                        .style("fill", color)
+
+        // let middleTooth: Rectangle = new Rectangle(new Point(0, 0), 10, 10, color);
+        // let lowerBlock: Rectangle = new Rectangle(new Point(-8, 10), 26, 20, color);
+
+        // this.svgComponent.call(lowerBlock.svgAppender.bind(lowerBlock))
+        // this.svgComponent.call(middleTooth.svgAppender.bind(middleTooth))
 
         this.makeDraggable() 
+    }
+
+    public select() {
+        super.select()
+        this.body.style("stroke", "yellow").style("stroke-width", "2")
+
+    }
+
+    public unselect() {
+        super.unselect()
+        this.body.style("stroke", null).style("stroke-width", null)
     }
 
     public dragAction(d) {
@@ -57,6 +74,12 @@ export class Relu extends Activation {
 
     constructor() {
         super("#736035")
+
+        this.svgComponent.append("path").attr("d", "M-5 20 l10 0 l7 -7")
+                                        .style("stroke", "black")
+                                        .style("stroke-width", 3)
+                                        .style("fill", "none")
+        
     }
 
     getHoverText(): string { return "relu" }
@@ -68,6 +91,12 @@ export class Sigmoid extends Activation {
 
     constructor() {
         super("#38001C")
+
+        this.svgComponent.append("path").attr("d", "M-5 20 l10 0 l7 -7")
+        .style("stroke", "black")
+        .style("stroke-width", 3)
+        .style("fill", "none")
+
     }
 
     getHoverText(): string { return "sigmoid" }
@@ -79,6 +108,12 @@ export class Softmax extends Activation {
     
     constructor() {
         super("#344743")
+
+        this.svgComponent.append("path").attr("d", "M-5 20 l10 0 l7 -7")
+        .style("stroke", "black")
+        .style("stroke-width", 3)
+        .style("fill", "none")
+        
     }
 
     getHoverText(): string { return "softmax" }
