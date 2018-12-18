040fb1bc9a8e5f6bb08ba0e33fbda4ae9655be7d
Zack Holbrook <zackh@mit.edu>
2018-12-18 11:56:41 -0500

    it was there

---
+++ src/model/export_model.ts
@@ -0,0 +1,32 @@
+import { DraggableData } from "../ui/app";
+import { Layer } from "../ui/shapes/layer";
+
+interface layerJson {
+    layer_name: string
+    id: number
+    children_ids: Array<number>
+    parent_ids: Array<number>
+    params: Map<string, any> 
+}
+
+function graphToJson(svgData: DraggableData): layerJson[] {
+	// Initialize queues, dags, and parents (visited) 
+	let queue: Layer[] = [svgData.input]
+	let visited: Set<Layer> = new Set()
+	let json: jsonModel = []
+	while (queue.length != 0) {
+		let current = queue.shift()
+		json.push(current.toJson())
+		// check each connections of the node
+		for (let child of current.children) {
+			if (!visited.has(child)) {
+				queue.push(child)
+				visited.add(child)
+			}
+		}
+    }
+    return json
+	console.log(json)
+}
+
+function jsonToPython()
