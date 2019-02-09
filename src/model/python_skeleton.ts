eb6533524bd75fbd6e5964871e72cbb2b22536fe
jmmichel <jmmichel@mit.edu>
2019-02-09 17:33:58 -0500

    added python_skeleton

---
+++ src/model/python_skeleton.ts
@@ -0,0 +1,62 @@
+import { model } from "./paramsObject";
+import { setModelHyperparameters } from "../ui/app";
+
+export function pythonSkeleton(model_code): string {
+    setModelHyperparameters();
+    return `from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Dense, Dropout, Flatten, Input, Concatenate
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+batch_size = ${model.params.batchSize}
+num_classes = 10
+epochs = ${model.params.epochs}
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+############################# Architecture made by Ennui
+${model_code}
+#############################
+
+model.compile(loss=keras.losses.${model.params.getPythonLoss()},
+              optimizer=keras.optimizers.${model.params.getPythonOptimizer()}(lr=${model.params.learningRate}),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+`
+}
