f61e419823bf5d87ff2cb6391c95e502ff6d2dcb
sgrosser <stefan.grosser1@gmail.com>
2019-02-18 13:57:38 -0500

    made the stripes of concat the same color

--- src/ui/shapes/layers/concatenate.ts
+++ src/ui/shapes/layers/concatenate.ts
@@ -8,7 +8,7 @@ export class Concatenate extends Layer {
 
     constructor(defaultLocation=Point.randomPoint(100, 40, ActivationLayer.defaultInitialLocation)) {
         super([new PathShape("M0 0 h23 v120 h-23 v-120 Z", '#F27493'), 
-               new PathShape("M0 38 h23 v4 h-23 v-4  Z", 'rgba(20, 20, 20, 0.35)'),
+               new PathShape("M0 38 h23 v4 h-23 v-4  Z", 'rgba(20, 20, 20, 0.2)'),
                new PathShape("M0 78 h23 v4 h-23 v-4  Z", 'rgba(20, 20, 20, 0.2)')], defaultLocation)
     }
 
