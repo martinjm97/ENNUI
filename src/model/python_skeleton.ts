7261063601bd163b287687579b2ba8a824b3bc78
jmmichel <jmmichel@mit.edu>
2019-02-22 18:16:45 -0500

    fix python bug

--- src/model/python_skeleton.ts
+++ src/model/python_skeleton.ts
@@ -8,7 +8,7 @@ import keras
 from keras.datasets import mnist
 from keras.models import Model
 from keras.layers import Dense, Dropout, Flatten, Input, Concatenate, BatchNormalization
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, ReLU
 from keras import backend as K
 
 batch_size = ${model.params.batchSize}
