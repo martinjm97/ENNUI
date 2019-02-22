8cdf9f12326781b6855d10b0b458bca5b6de0cec
sgrosser <stefan.grosser1@gmail.com>
2019-02-22 18:16:19 -0500

    added relu to python skeleton

--- src/model/python_skeleton.ts
+++ src/model/python_skeleton.ts
@@ -7,7 +7,7 @@ export function pythonSkeleton(model_code): string {
 import keras
 from keras.datasets import mnist
 from keras.models import Model
-from keras.layers import Dense, Dropout, Flatten, Input, Concatenate, BatchNormalization
+from keras.layers import Dense, Dropout, Flatten, Input, Concatenate, BatchNormalization, ReLU
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
